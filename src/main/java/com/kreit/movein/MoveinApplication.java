86677e26cf5281c653bccf808f1b23e697094a24
오한진 <han@villanema.com>
2023-12-25 22:32:30 +0900

    feat: add health check API

--- src/main/java/com/kreit/movein/MoveinApplication.java
+++ src/main/java/com/kreit/movein/MoveinApplication.java
@@ -3,6 +3,7 @@ package com.kreit.movein;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,5 +11,10 @@ public class MoveinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoveinApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "OK";
     }
 }
