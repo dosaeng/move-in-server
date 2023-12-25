0f4b679e99513553f04db999deac89596a971ff5
오한진 <han@villanema.com>
2023-12-25 23:41:47 +0900

    feat: add health check API

--- src/main/java/com/kreit/movein/MoveinApplication.java
+++ src/main/java/com/kreit/movein/MoveinApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RestController
 public class MoveinApplication {
 
     public static void main(String[] args) {
